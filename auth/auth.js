c6585df388fa9643129e9bbbf689e71d1ea68296
William Forsyth <williamforsyth@williams-air.home>
2024-04-21 12:37:22 +0100

    /

--- auth/auth.js
+++ auth/auth.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const userModel = require('../models/userModel.js');
 const pantryModel = require('../models/pantryModel.js');
 const adminModel = require('../models/adminModel.js');
